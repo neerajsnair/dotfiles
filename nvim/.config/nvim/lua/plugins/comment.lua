9031cfd003aff63715cfa8299af227e9ad27711e
neerajsnair <neerajsnair95@gmail.com>
2025-06-29 16:57:23 +0530

    plugin for nvim

---
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,18 @@
+return {
+  "numToStr/Comment.nvim",
+  config = function()
+    line = {
+      toggler = 'gcc',
+      block = 'gbc',
+    }
+    opleader = {
+      line = 'gc',
+      block = 'gb',
+    }
+    extra = {
+      above = 'gcO',
+      below = 'gco',
+      eol = 'gcA',
+    }
+  end
+}
